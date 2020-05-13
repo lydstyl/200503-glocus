0ab435c0923aaf4d24688a2de69cd6fa8057aeb4
Gabriel Brun <lydstyl@gmail.com>
2020-05-13 21:55:27 +0200

    SearchZone in progress

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Autosuggest from 'react-autosuggest';
 
 import WithContainer from '../../hocs/withContainer';
+import SearchZone from '../../components/SearchZone/SearchZone';
 
 // When suggestion is clicked, Autosuggest needs to populate the input
 // based on the clicked suggestion. Teach Autosuggest how to calculate the
@@ -15,69 +15,9 @@ const renderSuggestion = (suggestion) => <div>{suggestion.name}</div>;
 // add contacts (firstName, lastname, email, phone)
 // map les contacts --> nom ou prenom ou email ou tél
 
-class SearchZone extends React.Component {
-  constructor() {
-    super();
-
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
-    this.state = {
-      value: '',
-      suggestions: [],
-    };
-  }
-
-  onChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue,
-    });
-  };
-
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: this.props.getSuggestions(value),
-    });
-  };
-
-  // Autosuggest will call this function every time you need to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
-    });
-  };
-
-  render() {
-    const { value, suggestions } = this.state;
-
-    // Autosuggest will pass through all these props to the input.
-    const inputProps = {
-      placeholder: 'Type a programming language',
-      value,
-      onChange: this.onChange,
-    };
-
-    // Finally, render it!
-    return (
-      <Autosuggest
-        suggestions={suggestions}
-        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-        getSuggestionValue={getSuggestionValue}
-        renderSuggestion={renderSuggestion}
-        inputProps={inputProps}
-      />
-    );
-  }
-}
-
 export const Search = () => {
   // Imagine you have a list of languages that you'd like to autosuggest.
-  const languages = [
+  const suggestions = [
     {
       name: 'C',
       year: 1972,
@@ -99,7 +39,7 @@ export const Search = () => {
 
     return inputLength === 0
       ? []
-      : languages.filter(
+      : suggestions.filter(
           (lang) => lang.name.toLowerCase().slice(0, inputLength) === inputValue
         );
   };
