8f01e64ca68a28cd6bc2d080d51ff456fb365a3a
Gabriel Brun <lydstyl@gmail.com>
2020-05-17 00:31:13 +0200

    StyledSettings

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import WithContainer from '../../hocs/withContainer';
-import { InputField } from '../../components/InputField/InputField';
 import { thunkSetSettings } from '../../actions/settingsActions';
+import WithContainer from '../../hocs/withContainer';
+import { StyledSettings } from './StyledSettings';
+import { InputField } from '../../components/InputField/InputField';
 
 export const Settings = () => {
   const settings = useSelector((state) => state.settings);
@@ -41,37 +42,41 @@ export const Settings = () => {
 
   return (
     <WithContainer title="Paramètre d'affichage">
-      <InputField
-        onChange={handleInputChange}
-        type='number'
-        name='maxContactsToShow'
-        value={maxContactsToShow}
-        label="Nombre maximum de contacts a afficher sur la page d'accueil"
-        max='100'
-        min='1'
-      />
+      <StyledSettings>
+        <InputField
+          onChange={handleInputChange}
+          type='number'
+          name='maxContactsToShow'
+          value={maxContactsToShow}
+          label="Nombre maximum de contacts a afficher sur la page d'accueil"
+          max='100'
+          min='1'
+        />
 
-      <InputField
-        onChange={handleInputChange}
-        type='number'
-        name='showContactIfLastActivityOlderThen'
-        value={showContactIfLastActivityOlderThen}
-        label='Afficher uniquement ceux dont la dernière activité est vieille de x jour(s)'
-        max='365'
-        min='0'
-      />
+        <InputField
+          onChange={handleInputChange}
+          type='number'
+          name='showContactIfLastActivityOlderThen'
+          value={showContactIfLastActivityOlderThen}
+          label='Afficher uniquement ceux dont la dernière activité est vieille de x jour(s)'
+          max='365'
+          min='0'
+        />
 
-      <InputField
-        onChange={handleInputChange}
-        type='number'
-        name='maxActivitiesToShow'
-        value={maxActivitiesToShow}
-        label="Nombre maximum d'activitées à afficher sur la page détail d'un contact"
-        max='20'
-        min='1'
-      />
+        <InputField
+          onChange={handleInputChange}
+          type='number'
+          name='maxActivitiesToShow'
+          value={maxActivitiesToShow}
+          label="Nombre maximum d'activitées à afficher sur la page détail d'un contact"
+          max='20'
+          min='1'
+        />
 
-      <button onClick={handleSaveSettings}>Sauver</button>
+        <div className='button-box'>
+          <button onClick={handleSaveSettings}>Sauver</button>
+        </div>
+      </StyledSettings>
     </WithContainer>
   );
 };
---
+++ src/pages/Settings/StyledSettings.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { spaces } from '../../utils/cssVariables';
+
+export const StyledSettings = styled.div`
+  input {
+    width: auto;
+    max-width: 44px;
+  }
+
+  .button-box {
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
