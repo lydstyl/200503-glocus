d99fdb232837f19f02ee9eb06620b41bf5f87f64
Gabriel Brun <lydstyl@gmail.com>
2020-09-06 21:12:28 +0200

    fix target icon in contact page

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,25 +1,26 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams, useHistory } from "react-router-dom";
-import QRCode from "qrcode.react";
-import { exists } from "../../utils/exists";
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useParams, useHistory } from 'react-router-dom'
+import QRCode from 'qrcode.react'
+import { ImTarget } from 'react-icons/im'
+import { exists } from '../../utils/exists'
 
 import {
   thunkDeleteContact,
   thunkAddActivity,
-} from "../../actions/contactActions";
-import WithContainer from "../../hocs/withContainer";
-import { StyledContactPage } from "./StyledContactPage";
-import { ActivityItem } from "../../components/ActivityItem/ActivityItem";
+} from '../../actions/contactActions'
+import WithContainer from '../../hocs/withContainer'
+import { StyledContactPage } from './StyledContactPage'
+import { ActivityItem } from '../../components/ActivityItem/ActivityItem'
 
 export const ContactPage = () => {
-  const dispatch = useDispatch();
-  const settings = useSelector((state) => state.settings);
-  const contacts = useSelector((state) => state.contacts);
-  const { id } = useParams();
-  const history = useHistory();
+  const dispatch = useDispatch()
+  const settings = useSelector((state) => state.settings)
+  const contacts = useSelector((state) => state.contacts)
+  const { id } = useParams()
+  const history = useHistory()
 
-  const contact = contacts.filter((c) => c.id === id)[0];
+  const contact = contacts.filter((c) => c.id === id)[0]
 
   const {
     civility,
@@ -32,50 +33,50 @@ export const ContactPage = () => {
     description,
     linkedin,
     category,
-  } = contact;
+  } = contact
 
-  let { activities } = contact;
+  let { activities } = contact
 
   activities = activities
     .sort((a, b) => {
       if (new Date(a.createdAt) > new Date(b.createdAt)) {
-        return -1;
+        return -1
       } else {
-        return 1;
+        return 1
       }
     })
-    .slice(0, settings.maxActivitiesToShow);
+    .slice(0, settings.maxActivitiesToShow)
 
   const handleDeleteContact = () => {
-    dispatch(thunkDeleteContact(id));
+    dispatch(thunkDeleteContact(id))
 
-    history.push("/");
-  };
+    history.push('/')
+  }
 
   const handleAddactivity = () => {
-    dispatch(thunkAddActivity(contact));
-  };
+    dispatch(thunkAddActivity(contact))
+  }
 
   return (
-    <WithContainer title="Détail du contact">
+    <WithContainer title='Détail du contact'>
       <StyledContactPage>
-        <div className="section contact">
-          <div className="contact-header">
+        <div className='section contact'>
+          <div className='contact-header'>
             <h3>
-              {quality === "2" && (
-                <span className="star" role="img" aria-label="best quality">
-                  🎯
+              {quality === '2' && (
+                <span className='star' role='img' aria-label='best quality'>
+                  <ImTarget />
                 </span>
-              )}{" "}
-              {exists(company) && company.toUpperCase()} {civility}{" "}
-              {exists(firstName) && firstName}{" "}
+              )}{' '}
+              {exists(company) && company.toUpperCase()} {civility}{' '}
+              {exists(firstName) && firstName}{' '}
               {exists(lastName) && lastName.toUpperCase()}
             </h3>
-            <div className="category">{category}</div>
+            <div className='category'>{category}</div>
           </div>
 
-          <div className="contact-infos">
-            <div className="left">
+          <div className='contact-infos'>
+            <div className='left'>
               {exists(phone) && (
                 <p>
                   <a href={`tel:${phone}`}>{phone}</a>
@@ -90,31 +91,31 @@ export const ContactPage = () => {
 
               {exists(linkedin) && (
                 <p>
-                  <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                  <a href={linkedin} target='_blank' rel='noopener noreferrer'>
                     Site web
                   </a>
                 </p>
               )}
             </div>
 
-            <div className="right">
+            <div className='right'>
               <p>{description}</p>
             </div>
           </div>
 
-          <Link className="edit" to={`/ajouter-ou-modifier-un-contact/${id}`}>
+          <Link className='edit' to={`/ajouter-ou-modifier-un-contact/${id}`}>
             Modifier le contact
           </Link>
         </div>
 
-        <div className="section  activities-box">
-          <div className="activities-header">
+        <div className='section  activities-box'>
+          <div className='activities-header'>
             <h3>Activités</h3>
 
             <button onClick={handleAddactivity}>Ajouter</button>
           </div>
 
-          <div className="activities">
+          <div className='activities'>
             {activities &&
               activities.map((a) => (
                 <ActivityItem
@@ -126,13 +127,13 @@ export const ContactPage = () => {
           </div>
         </div>
 
-        <div className="section contact-footer">
-          <div className="qr-code-box">
+        <div className='section contact-footer'>
+          <div className='qr-code-box'>
             <QRCode
               value={`BEGIN:VCARD
 VERSION:4.0
-FN:${`${civility}${exists(firstName) && " " + firstName}${
-                exists(lastName) && " " + lastName.toUpperCase()
+FN:${`${civility}${exists(firstName) && ' ' + firstName}${
+                exists(lastName) && ' ' + lastName.toUpperCase()
               }`}
 ${exists(company) && `ORG:${company.toUpperCase()}`}
 ${exists(phone) && `TEL:${phone}`}
@@ -144,11 +145,11 @@ END:VCARD`}
 
           <hr />
 
-          <div className="delete-all-box">
+          <div className='delete-all-box'>
             <button onClick={handleDeleteContact}>Supprimer tout</button>
           </div>
         </div>
       </StyledContactPage>
     </WithContainer>
-  );
-};
+  )
+}
